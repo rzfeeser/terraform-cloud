a81b0173198fd3aa226f7d87241c41780b422550
RZFeeser <rzfeeser@users.noreply.github.com>
2022-10-05 01:32:24 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,34 @@
-/* a list of local variables */
-locals {
-  avengers = ["ironman", "captain america", "thor","doctor strange","spider man","hulk","black panther","black widow"]
-}
+    terraform {
+      required_providers {
+        null = {
+          source = "hashicorp/null"
+          version = "3.1.1"
+        }
+      }
+    }
+
+    provider "null" {
+      # Configuration options
+    }
+
+    /* a list of local variables */
+    locals {
+      jones = ["indy", "henry", "marian"] 
+    }
 
 
-/* The null_resource implements the standard resource lifecycle but takes no more action */
-resource "null_resource" "avengers" {
-  for_each = toset(local.avengers)
-  /* triggers allows specifying a random set of values that when
-     changed will cause the resource to be replaced */
-  triggers = {
-    name = each.value  // a special variable, "each" created by terraform
-                       // the object has "each.key" and "each.value"
-  }
-}
+    /* The null_resource implements the standard resource lifecycle but takes no more action */
+    resource "null_resource" "jones" {
+      for_each = toset(local.jones)
+      /* triggers allows specifying a random set of values that when
+         changed will cause the resource to be replaced */
+      triggers = {
+        name = each.value  // a special variable, "each" created by terraform
+                           // the object has "each.key" and "each.value"
+      }
+    }
 
-/* We want these outputs */
-output "avengers" {
-  value = null_resource.avengers
-}
+    /* We want these outputs */
+    output "jones" {
+      value = null_resource.jones
+    }

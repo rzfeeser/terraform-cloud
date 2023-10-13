4d6fe8e4d761b269d8acb1c980b35eea429546e5
RZFeeser <rzfeeser@users.noreply.github.com>
2023-10-13 10:52:19 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "null" {
 
 /* a list of local variables */
 locals {
-  jones = ["indy", "henry", "marian", "katanga", "sallah"] 
+  jones = ["indy", "henry", "marian", "katanga", "sallah", "marcus"] 
 }
 
 
